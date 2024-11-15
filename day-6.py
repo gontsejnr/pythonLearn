8f0f43011fa5d185a7b90277736174033de6c8cc
gontsejnr <gontsemaepa@gmail.com>
2024-11-15 23:47:13 +0200

    day 6

---
+++ day-6.py
@@ -0,0 +1,22 @@
+# functions: creating reusable code to call 
+
+# create/define a functions
+def my_function():
+    print('Hello')
+    print('Bye')
+
+# calling the created function at top
+my_function()
+
+# Indentation: making sure that the code is written correctly in indent
+
+# function block
+def myFunction():
+    # if block
+    if sky == 'clear':
+        print('blue')
+    # elif block 
+    elif sky == 'cloudy':
+        print('grey')
+    print('hello')
+print('world')            
